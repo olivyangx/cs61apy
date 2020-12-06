ce1605242311ed6b74a40283f0e9cf9705bfe201
Olivia Yang <74348145+olivyangx@users.noreply.github.com>
2020-12-06 09:57:46 +0800

    Update lab00.py

--- lab00.py
+++ lab00.py
@@ -10,7 +10,7 @@ def twenty_eighteen():
 	age = 20 #I am 20
 	country = 86 # from a country whose calling code is 86
 	world = 3  # Living in a third world country
-	gender = 2-1 # I am what it called le deuxième sexe, all I want is become a free individual
+	gender = 2-1 # I am what it called le deuxième sexe, all I want is to become a free individual
 	year = 2020 # This is the year of chaos 2020
 	days = 320 # It's been 320 days since I made up my decision to be a coder
 	me = (age + country)*world + (year - days)*gender 
