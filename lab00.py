50d44ce86efdd3086fda662e436d0446eb52e16c
Olivia Yang <74348145+olivyangx@users.noreply.github.com>
2020-12-06 09:54:40 +0800

    Add files via upload

---
+++ lab00.py
@@ -0,0 +1,18 @@
+def twenty_eighteen():
+    """Come up with the most creative expression that evaluates to 2018,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_eighteen()
+    2018
+    """
+    return ______
+def twenty_eighteen():
+	age = 20 #I am 20
+	country = 86 # from a country whose calling code is 86
+	world = 3  # Living in a third world country
+	gender = 2-1 # I am what it called le deuxième sexe, all I want is become a free individual
+	year = 2020 # This is the year of chaos 2020
+	days = 320 # It's been 320 days since I made up my decision to be a coder
+	me = (age + country)*world + (year - days)*gender 
+	return me
+
